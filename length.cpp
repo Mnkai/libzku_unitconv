7faf0b79eebaa7fffcb44038fbd3d37aa26a2c66
Minori Hiraoka <minori@mnetwork.co.kr>
2017-05-28 23:59:58 +0900

    Change raw length system to meter

--- length.cpp
+++ length.cpp
@@ -2,45 +2,49 @@
 
 class length {
 private:
-    // raw value for length is millimeter
+    // raw value for length is meter
     const long long double rawValue;
 
 public:
-    length(double i, lengthTypes t) {
+    length(long long double i, lengthTypes t) {
         // TODO: Get original length type, and convert to millimeter, and save
         switch (lengthTypes)
         {
             case millimeter:
-
+                rawValue = 0.001*i;
                 break;
             case centimeter:
-
+                rawValue = 0.01i;
                 break;
             case meter:
-
+                rawValue = i;
                 break;
             case kilometer:
-
+                rawValue = 1000*i;
                 break;
             case inch:
-
+                rawValue = 0.0254*i;
                 break;
             case feet:
-
+                rawValue = 0.3048*i;
                 break;
             case yard:
-
+                rawValue = 0.9144*i;
+                break;
+            case mile:
+                rawValue = 1609.34*i;
                 break;
         }
 
         rawValue = i;
     }
 
-    long long double toMillimeter(){return rawValue;}
-    long long double toCentimeter(){return 10*rawValue;}
-    long long double toMeter(){return 100*10*rawValue;}
-    long long double toKilometer(){return 1000*10*rawValue;}
-    long long double toInch(){return 0.0393701*rawValue;}
-    long long double toFeet(){return 0.00328084*rawValue;}
-    long long double toYard(){return 0.00109361*rawValue;}
+    long long double toMillimeter(){return 1000*rawValue;}
+    long long double toCentimeter(){return 100*rawValue;}
+    long long double toMeter(){return rawValue;}
+    long long double toKilometer(){return 0.001*rawValue;}
+    long long double toInch(){return 39.3701*rawValue;}
+    long long double toFeet(){return 3.28084*rawValue;}
+    long long double toYard(){return 1.09361*rawValue;}
+    long long double toMile(){return 0.000621371*rawValue;}
 };
